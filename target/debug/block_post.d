90b4a24b1532a62a8b49ce4b8a4bf1ac8d2072ae
Thomas-Martin-1998 <1998.thomas.martin@gmail.com>
2023-12-08 22:51:19 +0000

    IOTA 2.0

--- target/debug/block_post.d
+++
@@ -1,1 +0,0 @@
-/Users/t.martin/Desktop/Workspace/Scripts/block_post/target/debug/block_post: /Users/t.martin/Desktop/Workspace/Scripts/block_post/src/main.rs
--- target/release/block_post.d
+++
@@ -1,1 +0,0 @@
-/Users/t.martin/Desktop/Workspace/Scripts/block_post/target/release/block_post: /Users/t.martin/Desktop/Workspace/Scripts/block_post/src/main.rs
